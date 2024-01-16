07a3b2f3ed8ac6816870950bca175546b3db4eb0
Edvin Junell <edvin.junell@student.tabyenskilda.se>
2024-01-16 13:32:31 +0100

    Insatt en del javascript

---
+++ script.js
@@ -0,0 +1,7 @@
+function byttillbild() {
+    document.getElementById('img1').src = 'Bilder/Bild 2, 900 x 500.webp';
+}
+
+function byttilllogga() {
+    document.getElementById('img1').src = 'Bilder/Bild 1, 900 x 500.webp';
+}
